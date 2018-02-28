08e6363de9a97212f8d2c691a50327931a638830
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-02-28 15:39:44 +0100

    Stub for most libraries that we need.

---
+++ kde/pim/kasync/kasync.py
@@ -0,0 +1,20 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = "git://anongit.kde.org/kasync"
+        self.defaultTarget = 'master'
+        self.description = "Library for writing asynchronous code."
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kcoreaddons"] = "default"
+
+from Package.CMakePackageBase import *
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)
---
+++ kde/pim/kdav2/kdav2.py
@@ -0,0 +1,22 @@
+import info
+
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['master'] = "git://anongit.kde.org/kdav2"
+        self.defaultTarget = 'master'
+        self.description = "KDav library"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.runtimeDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["frameworks/tier1/kcoreaddons"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)

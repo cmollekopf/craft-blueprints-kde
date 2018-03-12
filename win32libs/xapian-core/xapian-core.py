6f5192cb1fc2acb1557388c76947aef7329ab3c8
kolab <kolab@ws14.local>
2018-03-12 10:49:03 +0100

    Build on osx

--- win32libs/xapian-core/xapian-core.py
+++ win32libs/xapian-core/xapian-core.py
@@ -4,12 +4,12 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['1.2.24'] = 'http://oligarchy.co.uk/xapian/1.2.24/xapian-core-1.2.24.tar.xz'
-        self.targetDigests['1.2.24'] = '5c63be033157e030f41e128956b873fbd9ad6a1c'
-        self.targetInstSrc['1.2.24'] = 'xapian-core-1.2.24'
+        self.targets['1.2.25'] = 'http://oligarchy.co.uk/xapian/1.2.25/xapian-core-1.2.25.tar.xz'
+        self.targetDigests['1.2.25'] = '4c305585c3f1d9f595eec875549406b4650fd29d'
+        self.targetInstSrc['1.2.25'] = 'xapian-core-1.2.25'
         self.description = "Open Source Search Engine library"
-        #self.patchToApply['1.2.24'] = [("xapian-core-1.2.24-20170626.diff", 1)]
-        self.defaultTarget = '1.2.24'
+        #self.patchToApply['1.2.25'] = [("xapian-core-1.2.24-20170626.diff", 1)]
+        self.defaultTarget = '1.2.25'
 
     def setDependencies(self):
         self.runtimeDependencies["win32libs/libxslt"] = "default"

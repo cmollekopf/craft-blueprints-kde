078ec5a5218e90e50b79a1e6d4c0ffbb98b08cf0
kolab <kolab@ws14.local>
2018-03-12 13:30:18 +0100

    Build xapian 1.4.5

--- win32libs/xapian-core/xapian-core.py
+++ win32libs/xapian-core/xapian-core.py
@@ -4,12 +4,12 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['1.2.25'] = 'http://oligarchy.co.uk/xapian/1.2.25/xapian-core-1.2.25.tar.xz'
-        self.targetDigests['1.2.25'] = '4c305585c3f1d9f595eec875549406b4650fd29d'
-        self.targetInstSrc['1.2.25'] = 'xapian-core-1.2.25'
+        self.targets['1.4.5'] = 'http://oligarchy.co.uk/xapian/1.4.5/xapian-core-1.4.5.tar.xz'
+        self.targetDigests['1.4.5'] = '56eca1467328794a406133c5f178b30f63bb992f'
+        self.targetInstSrc['1.4.5'] = 'xapian-core-1.4.5'
         self.description = "Open Source Search Engine library"
-        #self.patchToApply['1.2.25'] = [("xapian-core-1.2.24-20170626.diff", 1)]
-        self.defaultTarget = '1.2.25'
+        #self.patchToApply['1.4.5'] = [("xapian-core-1.2.24-20170626.diff", 1)]
+        self.defaultTarget = '1.4.5'
 
     def setDependencies(self):
         self.runtimeDependencies["win32libs/libxslt"] = "default"
@@ -26,3 +26,11 @@ class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
         #self.subinfo.options.configure.args += " --disable-static --enable-shared "
+        #print("INSTALL ARGS: " + self.subinfo.options.install.args)
+        #self.subinfo.options.install.args += " install "
+
+    def install(self):
+        #print("INSTALL ARGS during install: " + self.subinfo.options.install.args)
+        if not AutoToolsPackageBase.install(self):
+            return False
+        return self.copyToMsvcImportLib()

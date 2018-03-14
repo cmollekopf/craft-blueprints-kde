a347097877832bcd76a34e850c647cbd84e85c4c
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-03-15 00:13:09 +0100

    We need package config

--- kde/pim/kube/kube.py
+++ kde/pim/kube/kube.py
@@ -8,6 +8,7 @@ class subinfo(info.infoclass):
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/pkg-config"] = "default"
         self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
         self.buildDependencies["libs/qt5/qtbase"] = "default"
         self.runtimeDependencies["libs/qt5/qtgraphicaleffects"] = "default"

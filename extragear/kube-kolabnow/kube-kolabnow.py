a065cfc68bef99de8133e4938045ad2b00697676
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-01 16:58:41 +0200

    Added kube for kolab now

---
+++ extragear/kube-kolabnow/kube-kolabnow.py
@@ -0,0 +1,37 @@
+import info
+
+class subinfo(info.infoclass):
+    def setTargets(self):
+        self.svnTargets['kolabnow'] = "git://anongit.kde.org/kube|kolabnow"
+        self.defaultTarget = 'kolabnow'
+        self.description = "Kube for Kolab Now"
+
+    def setDependencies(self):
+        self.runtimeDependencies["virtual/base"] = "default"
+        self.buildDependencies["dev-util/pkg-config"] = "default"
+        self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
+        self.buildDependencies["libs/qt5/qtbase"] = "default"
+        self.runtimeDependencies["libs/qt5/qtgraphicaleffects"] = "default"
+        self.runtimeDependencies["libs/qt5/qtmultimedia"] = "default"
+        self.runtimeDependencies["libs/qt5/qtquickcontrols"] = "default"
+        self.runtimeDependencies["libs/qt5/qtquickcontrols2"] = "default"
+        self.runtimeDependencies["libs/qt5/qtsvg"] = "default"
+        self.runtimeDependencies["libs/qt5/qtwinextras"] = "default"
+        self.runtimeDependencies["libs/qt5/qtwebengine"] = "default"
+        self.buildDependencies["frameworks/tier1/kcoreaddons"] = "default"
+        self.buildDependencies["kde/pim/kmime"] = "default"
+        self.buildDependencies["kde/pim/kcalcore"] = "default"
+        self.buildDependencies["extragear/sink"] = "default"
+        self.buildDependencies["extragear/kasync"] = "default"
+        if CraftCore.compiler.isGCCLike():
+            self.runtimeDependencies["autotools/gpgme-src"] = "default"
+        else:
+            self.runtimeDependencies["win32libs/gpgme"] = "default"
+
+
+from Package.CMakePackageBase import *
+
+
+class Package(CMakePackageBase):
+    def __init__(self):
+        CMakePackageBase.__init__(self)

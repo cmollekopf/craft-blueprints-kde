9f6836eb26ddfe95300d680daba1607640e66608
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-13 23:46:46 +0200

    Apply patch

--- frameworks/tier1/kcoreaddons/kcoreaddons.py
+++ frameworks/tier1/kcoreaddons/kcoreaddons.py
@@ -4,8 +4,8 @@ import info
 class subinfo(info.infoclass):
     def setTargets(self):
         self.versionInfo.setDefaultValues()
-
         self.description = "KCoreAddons"
+        self.patchToApply["5.44.0"] = [("kcoreaddons-5.44.0-20180413.diff", 1)]
 
     def setDependencies(self):
         self.buildDependencies["virtual/base"] = "default"

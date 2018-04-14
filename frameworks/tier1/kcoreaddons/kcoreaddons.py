17b3bd9732f5dd895801ad4e2f9ac4875f1f659c
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-14 09:51:48 +0200

    Avoid building shared-mime-info

--- frameworks/tier1/kcoreaddons/kcoreaddons.py
+++ frameworks/tier1/kcoreaddons/kcoreaddons.py
@@ -11,7 +11,8 @@ class subinfo(info.infoclass):
         self.buildDependencies["virtual/base"] = "default"
         self.buildDependencies["frameworks/extra-cmake-modules"] = "default"
         self.runtimeDependencies["libs/qt5/qtbase"] = "default"
-        self.runtimeDependencies["win32libs/shared-mime-info"] = "default"
+        #Qt will fall back to it's own internal copy of shared-mime-info http://doc.qt.io/qt-5/qmimedatabase.html
+        #self.runtimeDependencies["win32libs/shared-mime-info"] = "default"
 
 
 from Package.CMakePackageBase import *

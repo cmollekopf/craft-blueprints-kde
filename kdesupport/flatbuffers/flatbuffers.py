97a349cb5bd6d53fc10ae81a7f1f3738fac1f444
Christian Mollekopf <mollekopf@kolabsystems.com>
2018-04-14 13:18:51 -0700

    Fixed build

--- kdesupport/flatbuffers/flatbuffers.py
+++ kdesupport/flatbuffers/flatbuffers.py
@@ -5,7 +5,7 @@ class subinfo(info.infoclass):
         self.svnTargets['master'] = "https://github.com/google/flatbuffers.git"
         self.defaultTarget = 'master'
         self.description = "flatbuffers"
-        self.patchToApply["master"] = ("flatbuffers-20180314.patch", 1) #Install flatc for RelWithDebInfo builds
+        self.patchToApply["master"] = ("flatbuffers-20180414.patch", 1) #Install flatc for RelWithDebInfo builds
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"

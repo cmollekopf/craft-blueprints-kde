e84a66be0d318f824579d512a97f84a480c6eacd
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-15 11:30:23 +0200

    Help the build process find zlib

--- win32libs/xapian-core/xapian-core.py
+++ win32libs/xapian-core/xapian-core.py
@@ -27,7 +27,7 @@ from Package.AutoToolsPackageBase import *
 class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
-        #self.subinfo.options.configure.args += " --disable-static --enable-shared "
+        self.subinfo.options.configure.args += ' LD="ld" LIBS="zlib/libz.a $LIBS"'
         #print("INSTALL ARGS: " + self.subinfo.options.install.args)
         #self.subinfo.options.install.args += " install "
 

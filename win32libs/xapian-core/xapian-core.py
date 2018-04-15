36b5aacfe9bea820ffb8f1cba2848e1b419bb372
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-15 11:18:53 +0200

    Build xapian 1.4.6

--- win32libs/xapian-core/xapian-core.py
+++ win32libs/xapian-core/xapian-core.py
@@ -4,12 +4,12 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['1.4.5'] = 'http://oligarchy.co.uk/xapian/1.4.5/xapian-core-1.4.5.tar.xz'
-        self.targetDigests['1.4.5'] = '56eca1467328794a406133c5f178b30f63bb992f'
-        self.targetInstSrc['1.4.5'] = 'xapian-core-1.4.5'
+        self.targets['1.4.6'] = 'http://oligarchy.co.uk/xapian/1.4.6/xapian-core-1.4.6.tar.xz'
+        self.targetDigests['1.4.6'] = '56eca1467328794a406133c5f178b30f63bb992f'
+        self.targetInstSrc['1.4.6'] = 'xapian-core-1.4.6'
         self.description = "Open Source Search Engine library"
-        self.patchToApply['1.4.5'] = [("xapian-core-1.4.5-20180320.diff", 1)]
-        self.defaultTarget = '1.4.5'
+        self.patchToApply['1.4.6'] = [("xapian-core-1.4.5-20180320.diff", 1)]
+        self.defaultTarget = '1.4.6'
 
     def setDependencies(self):
         self.runtimeDependencies["win32libs/libxslt"] = "default"

47c7c2a0d05daebf9e903a8c6bc52f07a93bf067
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-16 09:32:32 +0200

    Build from git so we know what we get.

--- win32libs/xapian-core/xapian-core.py
+++ win32libs/xapian-core/xapian-core.py
@@ -1,15 +1,11 @@
 import info
 
-#from Package.CMakePackageBase import *
-
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['1.4.6'] = 'http://oligarchy.co.uk/xapian/1.4.6/xapian-core-1.4.6.tar.xz'
-        self.targetDigests['1.4.6'] = '56eca1467328794a406133c5f178b30f63bb992f'
-        self.targetInstSrc['1.4.6'] = 'xapian-core-1.4.6'
+        self.svnTargets['1.4.5'] = 'https://github.com/xapian/xapian.git|Release/1.4'
         self.description = "Open Source Search Engine library"
-        self.patchToApply['1.4.6'] = [("xapian-core-1.4.5-20180320.diff", 1)]
-        self.defaultTarget = '1.4.6'
+        self.patchToApply['1.4.5'] = [("xapian-core-1.4.5-20180320.diff", 1)]
+        self.defaultTarget = '1.4.5'
 
     def setDependencies(self):
         self.runtimeDependencies["win32libs/libxslt"] = "default"
@@ -19,16 +15,13 @@ class subinfo(info.infoclass):
             self.buildDependencies["dev-util/msys"] = "default"
 
 
-#class Package(CMakePackageBase):
-#    def __init__(self, **args):
-#        CMakePackageBase.__init__(self)
-
 from Package.AutoToolsPackageBase import *
 
 class Package(AutoToolsPackageBase):
     def __init__(self, **args):
         AutoToolsPackageBase.__init__(self)
-        self.subinfo.options.configure.args += ' LD="ld" LIBS="zlib/libz.a $LIBS"'
+        if OsDetection.isWin():
+            self.subinfo.options.configure.args += " LD='ld' --disable-backend-remote"
         #print("INSTALL ARGS: " + self.subinfo.options.install.args)
         #self.subinfo.options.install.args += " install "
 

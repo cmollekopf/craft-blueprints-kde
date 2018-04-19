e00ac1dc09a28fd2a2fadbbf3ccac5fae08485b4
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-19 14:17:21 +0200

    It's libcurl not just curl

--- extragear/sink/sink.py
+++ extragear/sink/sink.py
@@ -20,7 +20,7 @@ class subinfo(info.infoclass):
         self.buildDependencies["kdesupport/flatbuffers"] = "default"
         self.buildDependencies["win32libs/lmdb"] = "default"
         self.buildDependencies["win32libs/xapian-core"] = "default"
-        self.buildDependencies["win32libs/curl"] = "default"
+        self.buildDependencies["win32libs/libcurl"] = "default"
 
 
 from Package.CMakePackageBase import *

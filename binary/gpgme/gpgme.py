ce1f9d8fecbf12747636e7dd427fb5b17681cc05
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-04-27 13:29:15 +0200

    Fixed gpgme package

--- binary/gpgme/gpgme.py
+++ binary/gpgme/gpgme.py
@@ -23,24 +23,6 @@ class PackageBin(BinaryPackageBase):
         self.subinfo.options.package.withCompiler = False
         self.subinfo.options.package.withSources = False
 
-    def unpack(self):
-        if not BinaryPackageBase.unpack(self): return False
-        installpath=os.path.join(self.imageDir(), "gpgme-1.10.0")
-        os.renames(os.path.join(installpath, "include"), os.path.join(self.imageDir(), "include"))
-        os.renames(os.path.join(installpath, "bin"), os.path.join(self.imageDir(), "bin"))
-        os.renames(os.path.join(installpath, "lib"), os.path.join(self.imageDir(), "lib"))
-        os.renames(os.path.join(installpath, "libexec"), os.path.join(self.imageDir(), "libexec"))
-        shutil.rmtree(installpath)
-        return True
-
-
-from Package.VirtualPackageBase import *
-
-if CraftCore.compiler.isMSVC():
-    class Package(PackageBin):
-        def __init__(self):
-            PackageBin.__init__(self)
-else:
-    class Package(VirtualPackageBase):
-        def __init__(self):
-            VirtualPackageBase.__init__(self)
+class Package(PackageBin):
+    def __init__(self):
+        PackageBin.__init__(self)

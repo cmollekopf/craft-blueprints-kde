18c057e5cb0f356bfe1e33dddbb096e1e2e2f30f
Christian Mollekopf <chrigi_1@fastmail.fm>
2018-05-01 21:18:52 +0200

    gpgme patch

--- binary/gpgme/gpgme.py
+++ binary/gpgme/gpgme.py
@@ -9,6 +9,8 @@ class subinfo(info.infoclass):
         for version in ['1.10.0']:
             self.targets[version] = "http://www.foobar.org/gpgme-1.10.0.7z"
         # self.targetDigests['1.10.0'] = '34df759e1ffe4acce301887007cccb62f9496ea0'
+        self.patch['1.10.0'] = '34df759e1ffe4acce301887007cccb62f9496ea0'
+        self.patchToApply["1.10.0"] = [("gpgme-20180427.diff", 1)]
 
         self.description = "gpgme"
         self.defaultTarget = '1.10.0'
@@ -16,6 +18,7 @@ class subinfo(info.infoclass):
     def setDependencies(self):
         self.runtimeDependencies["virtual/bin-base"] = "default"
         self.buildDependencies["win32libs/pthreads"] = "default"
+        self.runtimeDependencies["win32libs/mingw-crt4msvc"] = "default"
         self.runtimeDependencies["binary/libgpg-error"] = "default"
         self.runtimeDependencies["binary/libassuan"] = "default"
 

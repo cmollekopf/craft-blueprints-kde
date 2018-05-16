c56e2f4fad0669f1907b99e74fd0d4068db603e5
Christian Mollekopf <mollekopf@kolabsystems.com>
2018-05-16 10:31:17 -0700

    Updated lmdb

--- win32libs/lmdb/lmdb.py
+++ win32libs/lmdb/lmdb.py
@@ -4,14 +4,13 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        for ver in ['0.9.16']:
+        for ver in ['0.9.21']:
             self.targets[ver] = 'https://github.com/LMDB/lmdb/archive/LMDB_' + ver + '.tar.gz'
             self.targetInstSrc[ver] = 'lmdb-LMDB_' + ver + '/libraries/liblmdb'
-        self.patchToApply['0.9.16'] = [('lmdb-LMDB_0.9.16-20151004.diff', 3)]
-        self.targetDigests['0.9.16'] = '367182e1d9dbc314db76459a71be719209f131b4'
+            self.patchToApply[ver] = [('lmdb-LMDB_0.9.16-20151004.diff', 3)]
 
         self.description = 'in memory database from the openldap project'
-        self.defaultTarget = '0.9.16'
+        self.defaultTarget = '0.9.21'
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"

9f4f2b0e4fe37348438715634d28b62eb956caf1
Christian Mollekopf <mollekopf@kolabsys.com>
2019-04-06 13:00:59 +0200

    Build xapian from git repo

--- win32libs/xapian-core/xapian-core.py
+++ win32libs/xapian-core/xapian-core.py
@@ -2,11 +2,9 @@ import info
 
 class subinfo(info.infoclass):
     def setTargets(self):
-        self.targets['1.4.5'] = 'http://oligarchy.co.uk/xapian/1.4.5/xapian-core-1.4.5.tar.xz'
-        self.targetInstSrc['1.4.5'] = 'xapian-core-1.4.5'
-        self.description = "Open Source Search Engine library"
-        self.patchToApply['1.4.5'] = [("xapian-core-1.4.5-20180320.diff", 1), ("xapian-core-1.4.5-20180321.diff", 1)]
+        self.svnTargets['1.4.5'] = "https://github.com/cmollekopf/xapian-core.git|master"
         self.defaultTarget = '1.4.5'
+        self.description = "Open Source Search Engine library"
 
     def setDependencies(self):
         self.runtimeDependencies["virtual/base"] = "default"
@@ -18,21 +16,3 @@ from Package.CMakePackageBase import *
 class Package(CMakePackageBase):
     def __init__(self, **args):
         CMakePackageBase.__init__(self)
-
-#else:
-#    from Package.AutoToolsPackageBase import *
-#
-#    class Package(AutoToolsPackageBase):
-#        def __init__(self, **args):
-#            AutoToolsPackageBase.__init__(self)
-#            #if OsDetection.isWin():
-#            #    self.subinfo.options.configure.args += " LD='lld-link.exe' OBJDUMP='llvm-objdump.exe' LIBS='C:\\Users\\User\\CraftRoot\\lib\\zlib.lib' --disable-backend-remote"
-#            #We typically run into LNK1160 (commandline args too long because the .exp files in .libs/ are too large.
-#            #By supplying -fuse-ld=ldd-link.exe we can use another linker which doesn't run into that problem, but then we end up with error: entry point must be defined.
-#            # Fuck. This. Shit.
-#
-#        def install(self):
-#            #print("INSTALL ARGS during install: " + self.subinfo.options.install.args)
-#            if not AutoToolsPackageBase.install(self):
-#                return False
-#            return self.copyToMsvcImportLib()
